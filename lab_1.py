e9d31ea49a06742bb301c4fa49a7759bbedc61d0
danielge24-meet <139704392+danielge24-meet@users.noreply.github.com>
2023-07-17 18:04:40 +0300

    Update lab_1.py

--- lab_1.py
+++ lab_1.py
@@ -1,5 +1,8 @@
 def create_youtube_video (title, description):
-	youtube_video={"title":title, "description":description,"likes":0,"dislikes":0,"comments":{}}
+	hashtags =set()
+	for i in range(5):
+		hashtags.add(input("hashtags: "))
+	youtube_video={"title":title, "description":description,"likes":0,"dislikes":0,"comments":{},"hashtags":hashtags}
 	return youtube_video
 
 def like(youtubevideo):
@@ -12,6 +15,13 @@ def dislike(youtubevideo):
 		youtubevideo["dislikes"] += 1
 	return youtubevideo
 
+def similarity_to_video(vid1,vid2):
+	s_vid1=vid1["hashtags"]	
+	s_vid2=vid2["hashtags"]
+	similiar_set=s_vid1.intersection(s_vid2)
+	float_percentage=(len(similiar_set)/max(len(s_vid1),len(s_vid2)))*100
+	return float_percentage
+
 def add_comment(youtubevideo,username,comment_text):
 	youtubevideo["comments"][username]=comment_text
 	return youtubevideo
@@ -21,5 +31,12 @@ for i in range(495):
 	like(youtube_video)
 dislike (youtube_video)
 
-video = add_comment(youtube_video,input("what ifis your username"),input("what comment would you like to leave"))
+video = add_comment(youtube_video,input("what is your username"),input("what comment would you like to leave"))
 print(video)
+
+youtube_video2=create_youtube_video(input("what would you like the title to be"), input("what would you like the description to be"))
+video2= add_comment(youtube_video2,input("what is your username"),input("what comment would you like to leave"))
+pe=similarity_to_video(video,video2)
+
+
+print("the percentage of similirity is: "+str(pe)+"%")
